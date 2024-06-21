5a82524e0f858d65b286d14a33fae4f044427711
KeKeZMen <makeskex@yandex.ru>
2024-06-21 14:10:11 +0300

    remove main id

---
+++ prisma/migrations/20240621105210_remove_main_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `main_id` on the `templates` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `templates` DROP COLUMN `main_id`;
