836d72cb172f68e28def277fa335fe990faa33eb
KeKeZMen <makeskex@yandex.ru>
2024-06-21 10:17:43 +0300

    link to /

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,13 +1,16 @@
 import { Box, Container, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <Box width="100%" bgcolor="#2196F3" paddingY="23px" paddingX="42px">
       <Container maxWidth="xl">
-        <Typography variant="h1" color="#ffffff" fontWeight={500}>
-          Мой шаблоны
-        </Typography>
+        <Link href="/">
+          <Typography variant="h1" color="#ffffff" fontWeight={500} fontSize="20px">
+            Мой шаблоны
+          </Typography>
+        </Link>
       </Container>
     </Box>
   );
