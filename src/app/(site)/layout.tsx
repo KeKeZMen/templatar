cd261737730d609c81a7ff5e29f622fd56f8ed47
KeKeZMen <makeskex@yandex.ru>
2024-06-21 10:17:56 +0300

    fake auth

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,12 +1,19 @@
 import { ReactNode } from "react";
 import { Container } from "@mui/material";
 import { Header } from "@widgets";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
+  const name = cookies().get("name");
+  if (!name) redirect("/login");
+
   return (
     <>
       <Header />
-      <Container maxWidth="xl">{children}</Container>
+      <Container maxWidth="xl" sx={{ marginTop: "15px" }}>
+        {children}
+      </Container>
     </>
   );
 }
